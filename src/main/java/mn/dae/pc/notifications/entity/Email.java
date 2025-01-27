f33aecb1e6a33e109f26c098573c83bc0bbff895
Paul Carlton <paul.carlton414@gmail.com>
2025-01-27 15:48:35 +0000

    remove template management

--- src/main/java/mn/dae/pc/notifications/entity/Email.java
+++ src/main/java/mn/dae/pc/notifications/entity/Email.java
@@ -7,6 +7,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class Email {
-    private final String recipient;
+    private final String recipient; // Single recipient for PofC
     private final Map<String, String> data;
 }
