a46f0fe6a0e96a91ee06b5787c3be754d7231681
Paul Carlton <paul.carlton414@gmail.com>
2025-01-28 12:07:53 +0000

    tester updates

--- src/main/java/mn/dae/pc/notifications/service/NotificationService.java
+++ src/main/java/mn/dae/pc/notifications/service/NotificationService.java
@@ -7,9 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.datatype.databind.ObjectMapper;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,17 @@ public class NotificationService {
         String html = render.generateHTML(templateName, email.getData());
         Map<String, String> htmlData = HashMap.newHashMap(1);
         System.out.print(html);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(document);
+
+            // Print JSON to the console
+            System.out.println("Generated JSON:");
+            System.out.println(jsonOutput);
+
+        } catch (Exception e) {
+            System.out.println("Error creating JSON document: " + e.getMessage());
+        }
         htmlData.put("body", html );
         email.setData(htmlData);
         try {
